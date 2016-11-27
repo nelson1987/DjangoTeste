0bf8001b34322f5fb4d6797b79af2cbe30d57005
nelson1987 <nelson.ash@outlook.com>
2016-11-27 11:33:47 -0200

    FUNCIONAL

--- TesteDjango/TesteDjango/urls.py
+++ TesteDjango/TesteDjango/urls.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from django.conf.urls import *
 from app.forms import BootstrapAuthenticationForm
 from django.contrib.auth import views
-#import app.views
+import app.views
 
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
@@ -14,53 +14,32 @@ from django.contrib.auth import views
 # admin.autodiscover()
 
 urlpatterns = [
-    #url(r'^$', app.views.home, name="home"),
-    #url(r'^detail/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    #url(r'^contact$', views.contact, name="contact"),
-    #url(r'^about', views.about, name='about'),
-    #url(r'^login/$',
-    #    views.login,
-    #    {
-    #        'template_name': 'app/login.html',
-    #        'authentication_form': BootstrapAuthenticationForm,
-    #        'extra_context':
-    #        {
-    #            'title':'Log in',
-    #            'year':datetime.now().year,
-    #        }
-    #    },
-    #    name='login'),
-]
-
-#urlpatterns = patterns('',
-#    url(r'^about/([0-9]{4})/$', 'year_archive'),
+    url(r'^$', app.views.home, name='home'),
+    url(r'^detail/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^contact$', app.views.contact, name="contact"),
+    url(r'^about', app.views.about, name='about'),
+    url(r'^login/$',
+        views.login,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        views.logout,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
     
-#    # Examples:
-#    #url(r'^$', 'app.views.home', name='home'),
-#    #url(r'^contact$', 'app.views.contact', name='contact'),
-#    #url(r'^about', 'app.views.about', name='about'),
-#    #url(r'^login/$',
-#    #    'django.contrib.auth.views.login',
-#    #    {
-#    #        'template_name': 'app/login.html',
-#    #        'authentication_form': BootstrapAuthenticationForm,
-#    #        'extra_context':
-#    #        {
-#    #            'title':'Log in',
-#    #            'year':datetime.now().year,
-#    #        }
-#    #    },
-#    #    name='login'),
-#    #url(r'^logout$',
-#    #    'django.contrib.auth.views.logout',
-#    #    {
-#    #        'next_page': '/',
-#    #    },
-#    #    name='logout'),
-
-#    # Uncomment the admin/doc line below to enable admin documentation:
-#    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-#    # Uncomment the next line to enable the admin:
-#    # url(r'^admin/', include(admin.site.urls)),
-#)
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
--- TesteDjango/app/views.py
+++ TesteDjango/app/views.py
@@ -7,51 +7,40 @@ from django.http import *
 from django.template import loader
 from datetime import datetime
 
+def index(request):
+    output = ""
+    return HttpResponse(output)
+
 def home(request):
     """Renders the home page."""
     context_dict = {
         'title':'Home Page', 
         'year': datetime.now().year,                   
         'message':'Your application description page.',
-        'boldmessage': "I am bold font from the context"}
+        'boldmessage': "I am bold font from the context"
+        }
+    assert isinstance(request, HttpRequest)
     return render(request, 'app\index.html', context = context_dict)
-    #return render(request, 'app\index.html', context_dict)
-    #assert isinstance(request, HttpRequest)
-    #template = loader.get_template('app/WebPage1.html')
-    # context = {
-    #        'title':'Home Page',
-    #        'year':datetime.now().year,
-    #        'contexto': [1,2,4,6,8]
-    #    }
-    #return render(request,'app/WebPage1.html',context)
-    #    request,
-    #    'app/index.html',
-    #    context = context
-    #)
-    #return HttpResponse("Hello, world.  You're at the polls index.")
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def contact(request):
     """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(request,
-        'app/contact.html',
-        context_instance = RequestContext(request,
-        {
+    context_instance = {
             'title':'Contact',
             'message':'Your contact page.',
             'year':datetime.now().year,
-        }))
+        }
+    assert isinstance(request, HttpRequest)
+    return render(request,'app/contact.html',context = context_instance)
 
 def about(request):
     """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(request,
-        'app/about.html',
-        context_instance = RequestContext(request,
-        {
+    context_instance = {
             'title':'About',
             'message':'Your application description page.',
             'year':datetime.now().year,
-        }))
+        }
+    assert isinstance(request, HttpRequest)
+    return render(request,'app/about.html',context = context_instance)
